7e1889b5040b661d9ea6b0a54904e724d9f9c8aa
TONGARARIN <tongararin@TONGARARINnoMacBook-Air.local>
2026-02-20 20:57:47 +0900

    メモの一覧が表示される機能を実装しました

--- app.rb
+++ app.rb
@@ -5,15 +5,28 @@ require 'pathname'
 require 'securerandom'
 
 get '/' do
+  redirect '/memos'
+end
+
+get '/new-memo' do
   erb :new_memo
+end
+
+get '/memos' do
+  dir = Pathname.new('./json_files')
+  json_files = dir.glob('*.json')
+  @files_data = json_files.map do |json_file|
+    JSON.parse(json_file.read, symbolize_names: true)
+  end
+  erb :memos
 end
 
 post '/memos' do
   @title = params[:title]
   @content = params[:content]
-  json_file = params.to_json
-  file = File.open("./json_files/#{SecureRandom.uuid}.json", 'w')
+  json_file = JSON.pretty_generate(params)
+  file = File.open("./json_files/#{params[:id]}.json", 'w')
   file.write("#{json_file}")
   file.close
-  erb :show_memo
+  redirect '/memos'
 end
